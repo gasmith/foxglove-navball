bb9331a7743a0d483acd8540416abe66a1f0e0ac
Greg Smith <gasmith@foxglove.dev>
2025-03-24 16:36:27 -0700

    rename

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { ExtensionContext } from "@foxglove/extension";
 import { initNavballPanel } from "./NavballPanel";
 
 export function activate(extensionContext: ExtensionContext): void {
-  extensionContext.registerPanel({ name: "navball-panel", initPanel: initNavballPanel });
+  extensionContext.registerPanel({ name: "Navball", initPanel: initNavballPanel });
 }
